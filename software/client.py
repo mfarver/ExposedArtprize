44884453202c12a933ade7c159ae0ab283da68b3
Jamie Bliss <astronouth7303@gmail.com>
2014-09-07 23:13:36 -0400

    Got some of the layout going

--- software/client.py
+++ software/client.py
@@ -3,9 +3,12 @@
 Client that actually sends frames to LED strip
 """
 import socket
+import array
 from animations import Animations
 
-class _AnimationControl:
+LED_COUNT = 500*3
+
+class _AnimationClient:
 	"""
 	Talks to the network and changes the animation
 	"""
@@ -21,5 +24,40 @@ class _AnimationControl:
 		text = data.decode('utf-8')
 		if text.endswith('\n'):
 			text = text[:-1]
-		return text, {}
+		ani = Animations.__members__[text]
+		return ani, {}
+
+AnimationClient = _AnimationClient()
+
+class AnimationRunner:
+	anireg = {}
+	default = None
+
+	def __iter__(self):
+		current = self.default()
+		while True:
+			a = yield
+			if a is not None:
+				current.close()
+				current = a()
+			try:
+				frame = next(current)
+			except StopIteration:
+				current = self.default()
+			# TODO: Send frame
+
+
+def animation(ani: Animations, *, default=False):
+	def _(func):
+		AnimationRunner.anireg[ani] = func
+		if default:
+			AnimationRunner.default = func
+		return func
+	return _
+
+@animation(Animations.off, default=True)
+def ani_off():
+	while True:
+		yield b"\0" * LED_COUNT
+
 
